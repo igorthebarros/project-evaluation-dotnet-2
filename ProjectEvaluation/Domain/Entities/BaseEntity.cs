3cc2645c9fc704c61e403d9b65b2c0b89d81a854
igorthebarros <1igorhbarros@gmail.com>
2025-04-12 08:35:29 -0300

    :tada: Start project; Initial architecture

---
+++ ProjectEvaluation/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using Common.Validation;
+
+namespace Domain.Entities
+{
+    public class BaseEntity : IComparable<BaseEntity>
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public Task<IEnumerable<ValidationErrorDetail>> ValidateAsync()
+        {
+            return Validator.ValidateAsync(this);
+        }
+
+        public int CompareTo(BaseEntity other) 
+        {
+            if (other == null)
+                return 1;
+
+            return other!.Id.CompareTo(Id);
+        }
+    }
+}
---
+++ ProjectEvaluation/Infrastructure/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
