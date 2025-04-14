6a9d45461d98694f2154d6de36fa36fbae5bd07f
igorthebarros <1igorhbarros@gmail.com>
2025-04-14 18:57:57 -0300

    :sparkles: Add Order create command

---
+++ ProjectEvaluation/Application/Commands/Orders/Create/CreateOrderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Commands.Orders.Create
+{
+    public class CreateOrderProfile : Profile
+    {
+        public CreateOrderProfile() 
+        {
+            CreateMap<CreateOrderCommand, Order>();
+            CreateMap<Order, CreateOrderResult>();
+        }
+    }
+}
