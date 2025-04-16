166faea2c810f28269c1f8ed3ed9cc7dc9b9da59
igorthebarros <1igorhbarros@gmail.com>
2025-04-16 08:57:26 -0300

    :recycle: Remove unwanted validations

--- ProjectEvaluation/Application/Commands/Orders/Create/CreateOrderValidator.cs
+++ ProjectEvaluation/Application/Commands/Orders/Create/CreateOrderValidator.cs
@@ -5,11 +5,6 @@ namespace Application.Commands.Orders.Create
 {
     public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
     {
-        public CreateOrderValidator()
-        {
-            RuleFor(x => x.Company.CNPJ).SetValidator(new CNPJValidator());
-            RuleForEach(x => x.Company.ContactUser).SetValidator(new UserValidator());
-            RuleForEach(x => x.Company.Phones).SetValidator(new PhoneValidator());
-        }
+        public CreateOrderValidator() { }
     }
 }
